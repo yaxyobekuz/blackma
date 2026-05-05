a4a637e5e81158da7db0014cad63de1d27e80f07
yaxyobekuz <pubgn9642@gmail.com>
2026-05-05 18:03:47 +0500

    fix: add bottom padding to body class in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${nunitoSans.variable} antialiased mx-auto`}
+        className={`${geistSans.variable} ${geistMono.variable} ${nunitoSans.variable} antialiased mx-auto pb-28`}
       >
         {children}
         <BottomNavigation />
